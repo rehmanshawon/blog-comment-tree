bbfb31e69d706ae4df6b2163817ab9ef04922491
rehmanshawon <meghnadbadh@gmail.com>
2022-05-21 21:30:59 +0600

    Heroku Deploy

--- server/index.js
+++ server/index.js
@@ -11,18 +11,23 @@ const cors = require("cors");
 dotenv.config();
 // connect to the database
 connectDB();
-
+const path = require("path");
 const app = express();
 app.use(express.json());
 
 app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.use("/api/users", userRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/account", passwordResetRoutes);
-
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 5000;
